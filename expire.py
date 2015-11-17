0d42726d1c650e4d50449d4de37b9051a5adb0ed
seoyujin <mildtuna555@gmail.com>
2015-11-17 15:57:42 +0900

    git ignore modified

--- expire.py
+++ expire.py
@@ -16,16 +16,12 @@ def convert_date(date_str):
         
 def expire():
 
-    #expired_date = date.today()
-    expired_date = date.today() + timedelta(days = +7 )
-    
+    expired_date = date.today()
 
     os_path = '/home/yujin/jigsaw_credentials_server/'
     cre_path = os_path + 'datas/credentials/' 
     cre_list = os.listdir(cre_path)
 
-    #f = open(os_path + '/aaa_test.txt', 'w')
-    #f.write(str(expired_date) + '\n')
     for cre in cre_list:
         store = oauth2client.file.Storage(cre_path + cre)
         service = credentials_mgr.get_service(store)
@@ -38,18 +34,13 @@ def expire():
             if file['title'] == 'jigsaw':
                 jigsaw_files[file['id']] = file
 
-        #f.write(cre + '\n')
         for file in all_files:
             if file['mimeType'] == 'application/vnd.google-apps.folder':
-                #f.write(file['title']+ '    ' + file['mimeType'] + '    ' + file['parents'][0]['id'] +  '\n')
                 if file['parents'][0]['id'] in jigsaw_files:
                     folder_date = convert_date(file['title'])
                     if folder_date:
                         if folder_date <= expired_date:
                             service.files().delete(fileId=file['id']).execute()
-
-    #f.close()
-
 
 
 if __name__ == '__main__':
